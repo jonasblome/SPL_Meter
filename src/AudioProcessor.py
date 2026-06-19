f7532a5dc6a4e5be068071e4fe94be8f8d261ca2
Jonathan Ploch <j.ploch@campus.tu-berlin.de>
2026-06-19 11:47:43 +0200

    Update AudioProcessor.py

--- src/AudioProcessor.py
+++ src/AudioProcessor.py
@@ -1,8 +1,13 @@
 import numpy as np
 
 class AudioProcessor:
-    def __init__(self):
+    def __init__(self, sample_rate=48000):
         print("AudioProcessor: Initializing")
+
+        self.sample_rate = sample_rate
+
+        self.fast_state = 0.0
+        self.slow_state = 0.0
 
     def compute_rms(self, audio_data):
         return np.sqrt(np.mean(audio_data**2))
@@ -16,3 +21,43 @@ class AudioProcessor:
         spl_db = 20 * np.log10(rms / reference_pressure)
         
         return spl_db
+    
+
+    #Zeitbewertung in fast and slow
+    def compute_time_weighted_factor(self, tau):
+        return np.exp(-1.0 / (self.sample_rate * tau))
+    
+    def update_time_weighting_state(self, sample, old_state, tau):
+        a = self.compute_time_weighting_factor(tau)
+
+        current_squared_pressure = sample ** 2
+
+        new_state = a * old_state + (1 - a) * current_squared_pressure
+
+        return new_state
+    
+    def process_time_weighting_block(self, audio_data, old_state, tau):
+        state = old_state
+
+        for sample in audio_data:
+            state = self.update_time_weighting_state(sample, state, tau)
+
+        return state
+    
+    def compute_fast_state(self, audio_data):
+        self.fast_state = self.process_time_weighting_block(
+            audio_data,
+            self.fast_state,
+            tau=0.125
+        )
+
+        return self.fast_state
+
+    def compute_slow_state(self, audio_data):
+        self.slow_state = self.process_time_weighting_block(
+            audio_data,
+            self.slow_state,
+            tau=1.0
+        )
+
+        return self.slow_state
