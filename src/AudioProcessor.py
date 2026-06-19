- added octave band a weightings dict - fixed design method of a weighting filterbank - added method to apply filterbank to a signal, - added method to compute a weighting from filtered signal - added test calls for a weighting and octave filterband spl values

src/AudioProcessor.py
@@ -1,8 +1,9 @@
 import numpy as np
+from scipy import signal
 
 class AudioProcessor:
     # According to IEC61672:2014
-    frequency_weights = {
+    frequency_weights_3rd_octave = {
         6.3: -85.4,
         8: -77.8,
         10: -70.4,
@@ -41,6 +42,21 @@ class AudioProcessor:
         20000: -9.3
     }
 
+    frequency_weights_octave = {
+        8: -77.8,
+        16: -56.7,
+        31.5: -39.4,
+        63: -26.2,
+        125: -16.1,
+        250: -8.6,
+        500: -3.2,
+        1000: 0.0,
+        2000: 1.2,
+        4000: 1.0,
+        8000: -1.1,
+        16000: -6.6
+    }
+
     def __init__(self):
         print("AudioProcessor: Initializing")
 
@@ -57,12 +73,46 @@ class AudioProcessor:
         
         return spl_db
     
-    def design_a_weighting(self, sample_rate):
-        filter_coefficients = np.array([10**(gain / 20) for gain in self.frequency_weights.values()])
-        
-        return filter_coefficients
-        
-    def apply_a_weighting(self, audio_data, sample_rate):
-        filter_coeffs = self.design_a_weighting(sample_rate)
+    def design_a_weighting_filterbank(self, sample_rate):
+        octave_ratio = 10**(3/10) # 3 dB pro Oktave entspricht einem Faktor von 10^(3/10)
+        band_lower_freqs = np.array(list(self.frequency_weights_octave.keys())) * octave_ratio**(-1/2)
+        band_upper_freqs = np.array(list(self.frequency_weights_octave.keys())) * octave_ratio**(1/2)
 
-        return np.convolve(audio_data, filter_coeffs, mode='same')
+        # nyquist_freq = sample_rate / 2 - 1
+        # band_upper_freqs = np.minimum(band_upper_freqs, nyquist_freq)
+        for lower_freq, upper_freq in zip(band_lower_freqs, band_upper_freqs):
+            if upper_freq >= sample_rate / 2:
+                raise ValueError(f"Sample rate {sample_rate} Hz is too low for the designed A-weighting filterbank. Please use a higher sample rate.")
+
+        a_weighting_filterbank = []
+        for lower_freq, upper_freq in zip(band_lower_freqs, band_upper_freqs):
+            sos_sections = signal.butter(
+                10,
+                [lower_freq, upper_freq],
+                btype='bandpass',
+                fs=sample_rate,
+                output='sos'
+            )
+            a_weighting_filterbank.append(sos_sections)
+        
+        return a_weighting_filterbank
+    
+    def apply_filterbank(self, audio_data, filterbank):
+        filtered_signals = []
+        for sos in filterbank:
+            filtered_signal = signal.sosfilt(sos, audio_data)
+            filtered_signals.append(filtered_signal)
+        
+        return filtered_signals
+        
+    def compute_a_weighting(self, filtered_signals):
+        weighted_signal = np.zeros_like(filtered_signals[0])
+        for i, filtered_signal in enumerate(filtered_signals):
+            band_center_freq = list(self.frequency_weights_octave.keys())[i]
+            weight_db = self.frequency_weights_octave[band_center_freq]
+            weight_linear = 10 ** (weight_db / 20)
+            weighted_signal += filtered_signal * weight_linear
+        
+        spl_db = self.compute_spl_db(weighted_signal)
+
+        return spl_db